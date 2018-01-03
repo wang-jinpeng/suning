6cdf9e6903a5cf30e5f6840269106904f0f78514
wang-jinpeng <857245460@qq.com>
2018-01-03 08:30:44 +0800

    suning

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
+//轮播图
 {
     let box1=document.querySelectorAll(".photo li");
     let box2=document.querySelectorAll(".banner-diandian li");
     let box=document.querySelector(".banner-photo");
     let left=document.querySelector(".bannerleft");
-    console.log(left)
+    console.log(left);
     let right=document.querySelector(".bannerright");
     box2.forEach(function (ele,index) {
         ele.onmouseover=function () {
@@ -55,10 +56,85 @@
             bannerfn("l");
         }
     };
-    box1.forEach(function (ele,index) {
+    box1.forEach(function (ele) {
         ele.addEventListener("transitionend",
             function(){
                 flag=true;
             })
     })
 }
+
+//侧导航、顶部搜索
+{
+    let daohang=document.querySelector(".leftdh");
+    let sousuo=document.querySelector("header");
+    let btns=document.querySelectorAll(".leftdh ul li");
+    let floors=document.querySelectorAll(".shoujidiannao");
+    let flag=true;
+    window.onscroll=function () {
+        if(flag){
+        let st=document.documentElement.scrollTop;
+        if (st>2180){
+            daohang.style.display="block"
+        }else {
+            daohang.style.display="none"
+        }
+        if (st>1000){
+            sousuo.style.display="block"
+        }else {
+            sousuo.style.display="none"
+        }
+        floors.forEach(function (ele,index) {
+            if(st<floors[0].offsetTop){
+                btns[0].classList.add("active");
+                for(let i=0;i<btns.length;i++){
+                    btns[i].classList.remove("active")
+                }
+                btns[0].classList.add("active")
+            }
+            if(st>=ele.offsetTop){
+                    btns[0].classList.add("active");
+                for(let i=0;i<btns.length;i++){
+                    btns[i].classList.remove("active")
+                }
+                btns[index+1].classList.add("active")
+            }
+        })
+        }
+    };
+    btns.forEach(function (ele,index) {
+        ele.onclick=function () {
+            flag=false;
+            let ot=floors[index].offsetTop;
+            let now=document.documentElement.scrollTop;
+            let speed=(ot-now)*30/300;
+            let time=0;
+            let t=setInterval(function () {
+                now+=speed;
+                time+=30;
+                if (time===300){
+                    clearInterval(t);
+                    now=ot;
+                    flag=true;
+                }
+                document.documentElement.scrollTop=now;
+            },30)
+        }
+    });
+    let totop=document.querySelector(".zhiding");
+    console.log(totop);
+    totop.onclick=function () {
+        let st=document.documentElement.scrollTop;
+        let speed=st*30/500;
+        let t=setInterval(function () {
+            st-=speed;
+            if(st<=0){
+                st=0;
+                clearInterval(t);
+            }
+            document.documentElement.scrollTop=st;
+        },30)
+    };
+}
+
+
